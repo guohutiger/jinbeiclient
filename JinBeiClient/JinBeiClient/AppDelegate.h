d3434da7cad2112c6e0b7b974a4b0686efcc7d4c
guohu <User@Tiger.local>
2014-04-14 20:12:42 +0800

    jibeiclient first version

---
+++ JinBeiClient/JinBeiClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JinBeiClient
+//
+//  Created by guohu on 14-4-14.
+//  Copyright (c) 2014年 jbm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JinBeiClient/JinBeiClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JinBeiClient
+//
+//  Created by guohu on 14-4-14.
+//  Copyright (c) 2014年 jbm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
